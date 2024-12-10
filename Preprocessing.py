e97c127ee47175f300244eb1543523f4d8188138
Damir Demirovic <138410671+ddemir2@users.noreply.github.com>
2024-12-10 14:23:52 -0800

    Update Preprocessing.py
    fixed bug in crop_image() to more accurate

--- Preprocessing.py
+++ Preprocessing.py
@@ -4,6 +4,7 @@ import numpy as np
 import os
 import math
 
+
 def summary_statistics(root_dir):
     """
     Across all images contained in root_dir: calculates minimum, average, and maximum widths and heights.
@@ -31,7 +32,8 @@ def summary_statistics(root_dir):
     image_widths = np.array(image_widths)
     image_heights = np.array(image_heights)
     min_width_height = (image_widths.min(), image_heights.min())
-    avg_width_height = (image_widths.mean(), image_heights.mean())
+    larger_avg_dimension = max(math.ceil(image_widths.mean()), math.ceil(image_heights.mean())) 
+    avg_width_height = (larger_avg_dimension, larger_avg_dimension)
     max_width_height = (image_widths.max(), image_heights.max())
 
     # package statistics together into a dictionary; return
@@ -60,7 +62,7 @@ def crop_image(image_path, target_width, target_height):
         center = float(orig_image_width) / 2.0
         left = center - (float(target_width) / 2.0)
         right = left + target_width
-        #print("width:  center is " + str(center) + ", left is " + str(left) + ", right is " + str(right))
+        print("width:  center is " + str(center) + ", left is " + str(left) + ", right is " + str(right))
     else:
         left = 0
         right = target_width
@@ -70,13 +72,14 @@ def crop_image(image_path, target_width, target_height):
         center = float(orig_image_height) / 2.0
         upper = center - (float(target_height) / 2.0)
         lower = upper + target_height
-        #print("height:  center is " + str(center) + ", lower is " + str(lower) + ", upper is " + str(upper))
+        print("height:  center is " + str(center) + ", lower is " + str(lower) + ", upper is " + str(upper))
     else:
         lower = target_height
         upper = 0
 
     # crop and return image
-    coordinates = (246.5,247.5 , 344.5,  343.5)
+    coordinates = (left, upper, right, lower)
+    #coordinates = (246.5, 247.5 , 344.5,  343.5)
     new_image = image.crop(coordinates)
     return new_image
 
@@ -113,7 +116,7 @@ def expand_image(image_path, target_width, target_height):
     # overlay original image over the white canvas and return
     canvas.paste(image, (left, upper))
     return canvas
-    
+
 
 def process_dataset(orig_dataset_path, processing_function, dest_dataset_path, new_image_prefix, new_width, new_height):
     """
@@ -149,7 +152,3 @@ def process_dataset(orig_dataset_path, processing_function, dest_dataset_path, n
             new_image_object = processing_function(orig_image_path, new_width, new_height)
             new_image_path = dest_dataset_path + orig_folder + "/" + new_image_prefix + orig_image
             new_image_object.save(new_image_path)
-
-
-dataset_statistics = summary_statistics('dataset/')
-process_dataset('dataset/', expand_image, 'dataset_expanded/', 'expanded_', dataset_statistics['max dimensions'][0], dataset_statistics['max dimensions'][1])
